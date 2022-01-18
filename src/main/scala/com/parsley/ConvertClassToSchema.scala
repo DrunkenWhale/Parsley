fc0fdb5944c20a733b995987528292c958f6cef2
pigeon_gugugu <892197240@qq.com>
2022-01-18 23:59:03 +0800

    refactor: ConvertClassToSchema

--- src/main/scala/com/parsley/ConvertClassToSchema.scala
+++ src/main/scala/com/parsley/ConvertClassToSchema.scala
@@ -3,12 +3,14 @@ package com.parsley
 import java.lang.reflect.Field
 import com.parsley.schema.Table
 
+import scala.annotation.Annotation
+
 class ConvertClassToSchema {
 
 }
 
 object ConvertClassToSchema {
-    def create(clazz: Class[_]): Unit = {
+    def create(clazz: Class[_]): String = {
         clazz.getDeclaredFields.foreach(x => println(x.getType.getName.toString + " ||| " + x.getName))
         clazz.getDeclaredFields.foreach(x => x.getDeclaredAnnotations.foreach(y => println("+++" + y + "\n")))
         clazz.getAnnotations.foreach(x => println(x.annotationType()))
@@ -17,32 +19,45 @@ object ConvertClassToSchema {
 
         // if this class not in any class
         // emmm will be exception
-        val packagePrefix = clazz.getPackageName + "."
-        val schemaName = clazz.getDeclaredAnnotation(classOf[Table])
-        match {
-            case null => clazz.getName.split(packagePrefix)(1).toLowerCase()
-            case x =>
-                if (x.schemaName() == "") clazz.getName.split(packagePrefix)(1).toLowerCase()
-                else x.schemaName().toLowerCase()
-        }
-        println(schemaName + "[]")
+
+        val schemaName = getSchemaName(clazz)
 
         //******************************************* schema column *******************************************
 
         val fieldsArray = clazz.getDeclaredFields
-        fieldsArray.foreach(x=>
-            x.getDeclaredAnnotations.foreach(y=>
-                println(y.annotationType().getName)
-            )
-        )
+        for (field <- fieldsArray) {
+            val annotations = field.getDeclaredAnnotations
+            field.getDeclaredAnnotation()
+        }
         //        clazz.getName.toLowerCase
+
+
+        "CREATE TABLE IF NOT EXISTS" + schemaName + "("
+
+        +");"
     }
 
-    val mapToSqlDataType = (field: Field) => field.getType.toString match
+    private val getSchemaName = (clazz: Class[_]) => {
+        clazz.getDeclaredAnnotation(classOf[Table])
+        match {
+            case null => getPackageSplitArray(clazz)
+            case x =>
+                if (x.schemaName() == "") getPackageSplitArray(clazz)
+                else x.schemaName().toLowerCase()
+        }
+    }
+
+    private val getPackageSplitArray = (clazz: Class[_]) => {
+        val packageSplitArray = clazz.getName.split("\\.")
+        packageSplitArray(packageSplitArray.length - 1).toLowerCase()
+    }
+
+    private val mapFieldToSqlSentence = (field: Field) => field.toString match
         case "java.lang.String" => "TEXT"
         case "int" => "INT"
         case "long" => "BIGINT"
         case "float" => "FLOAT"
         case "double" => "DOUBLE"
+        case _ => throw Exception("Unknown SQL Type")
 
 }
