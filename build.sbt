50613700d97d4c27f94d93c19a7046dbe93b9616
pigeon_gugugu <892197240@qq.com>
2022-02-23 20:49:53 +0800

    feat: add instanceBySeq && finish test

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "Parsley"
 
 version := "0.1"
 
-scalaVersion := "3.0.2"
+scalaVersion := "3.1.1"
 // https://mvnrepository.com/artifact/mysql/mysql-connector-java
 libraryDependencies ++= Seq(
     "mysql" % "mysql-connector-java" % "8.0.25",
--- src/main/scala/com/parsley/macroImpl/InstanceBySeq.scala
+++ src/main/scala/com/parsley/macroImpl/InstanceBySeq.scala
@@ -2,17 +2,24 @@ package com.parsley.macroImpl
 
 import scala.quoted.*
 
-inline def instanceBySeq[T](seq: Seq[Any]) = $ {instanceBySeqImpl[T](seq)}
+inline def instanceBySeq[T](inline seq: Seq[Any]): T = $ {
+    instanceBySeqImpl[T]('seq)
+}
 
-
-private def instanceBySeqImpl[T](seqExpr: Seq[Any])(using quotes: Quotes, typ: Type[T]) = {
+private def instanceBySeqImpl[T](x: Expr[Seq[Any]])(using quotes: Quotes, typ: Type[T]): Expr[T] = {
     import quotes.reflect.*
-    val seq: Seq[?] = seqExpr
+    val tree = x.asTerm
+    val typeList = tree match {
+        case Inlined(_, _, Apply(_, args)) => args
+    }
+    val termList = typeList.head match {
+        case Typed(Repeated(tl, _), _) => tl
+    }
     Apply(
         Select(
             New(TypeTree.of[T]),
             TypeRepr.of[T].typeSymbol.primaryConstructor
         ),
-        seq.map(x=>Expr(x).asTerm.tree)
+        termList
     ).asExprOf[T]
 }
