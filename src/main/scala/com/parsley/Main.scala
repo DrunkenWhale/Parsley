4ac643d1cce93d2547a44a5ccdb39ed73729f568
pigeon_gugugu <892197240@qq.com>
2022-02-23 23:31:30 +0800

    style(PrimaryConstructorParamList.scala): Rename PrimaryConstructor => PrimaryConstructorParamList

--- src/main/scala/com/parsley/Main.scala
+++ src/main/scala/com/parsley/Main.scala
@@ -1,12 +1,12 @@
 package com.parsley
 
-import com.parsley.macroImpl.primaryConstructor
+import com.parsley.macroImpl.primaryConstructorParamList
 import com.parsley.macroImpl.instanceBySeq
 
 case class A(name:String)
 
 @main def main(): Unit ={
-    println(primaryConstructor[A])
+    println(primaryConstructorParamList[A])
 }
 
 @main def test1(): Unit ={
--- src/main/scala/com/parsley/macroImpl/PrimaryConstructorParamList.scala
+++ src/main/scala/com/parsley/macroImpl/PrimaryConstructorParamList.scala
@@ -2,9 +2,9 @@ package com.parsley.macroImpl
 
 import scala.quoted.*
 
-inline def primaryConstructor[T]: List[Tuple2[String, String]] = ${primaryConstructorImpl[T]}
+inline def primaryConstructorParamList[T]: List[Tuple2[String, String]] = ${primaryConstructorParamListImpl[T]}
 
-private def primaryConstructorImpl[T](using quotes: Quotes, typed: Type[T]): Expr[List[Tuple2[String, String]]] = {
+private def primaryConstructorParamListImpl[T](using quotes: Quotes, typed: Type[T]): Expr[List[Tuple2[String, String]]] = {
     import quotes.reflect.*
     Expr(TypeTree.of[T].symbol.primaryConstructor.paramSymss.head.map(x => x.tree match {
         case ValDef(name, tp, _) => (name, tp.tpe.show)
