871bc4ed87172fa9f5131aa8b2e0436654e60f4a
pigeon_gugugu <892197240@qq.com>
2022-02-24 23:47:36 +0800

    fix(primaryConstructorParamList):remove implicit class tag

--- src/main/scala/com/parsley/macroImpl/PrimaryConstructorParamList.scala
+++ src/main/scala/com/parsley/macroImpl/PrimaryConstructorParamList.scala
@@ -10,8 +10,8 @@ inline def primaryConstructorParamList[T]: List[(String, String)] = $ {
 private def primaryConstructorParamListImpl[T](using quotes: Quotes, typed: Type[T]): Expr[List[(String, String)]] = {
     import quotes.reflect.*
     Expr(TypeTree.of[T].symbol.primaryConstructor.paramSymss.head.map(x => x.tree match {
-            case ValDef(name, tp, _) => (name, tp.tpe.show)
-            case x => throw new Exception(s"Illegal Constructor => $x")
-        }))
+        case ValDef(name, tp, _) => (name, tp.tpe.show)
+        case x => throw new Exception(s"Illegal Constructor => $x")
+    }))
 
 }
